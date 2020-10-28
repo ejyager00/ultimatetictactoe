99d5af6ccfc7fec0d2fd339706b0902678026d16
Eric Yager <ejyager00@gmail.com>
2020-10-27 22:37:03 -0500

    added similarity checks

--- ultimate_tic_tac_toe_bits.py
+++ ultimate_tic_tac_toe_bits.py
@@ -21,12 +21,6 @@ VICTORIES=(
     Bits(bin='0b001010100'), #diagonal /
 )
 
-ORDER_FOR_TO_STRING = [0, 1, 2, 9, 10, 11, 18, 19, 20, 3, 4, 5, 12, 13, 14, 21,
-22, 23, 6, 7, 8, 15, 16, 17, 24, 25, 26, 27, 28, 29, 36, 37, 38, 45, 46, 47, 30,
-31, 32, 39, 40, 41, 48, 49, 50, 33, 34, 35, 42, 43, 44, 51, 52, 53, 54, 55, 56,
-63, 64, 65, 72, 73, 74, 57, 58, 59, 66, 67, 68, 75, 76, 77, 60, 61, 62, 69, 70,
-71, 78, 79, 80]
-
 def possible_moves(gamestate):
     moves=[]
     lastmove=9
@@ -91,6 +85,11 @@ def make_move(gamestate, move):
     return Bits(bin=new_bin)
 
 def gamestate_to_string(gamestate):
+    ORDER_FOR_TO_STRING = [0, 1, 2, 9, 10, 11, 18, 19, 20, 3, 4, 5, 12, 13, 14,
+    21, 22, 23, 6, 7, 8, 15, 16, 17, 24, 25, 26, 27, 28, 29, 36, 37, 38, 45, 46,
+    47, 30, 31, 32, 39, 40, 41, 48, 49, 50, 33, 34, 35, 42, 43, 44, 51, 52, 53,
+    54, 55, 56, 63, 64, 65, 72, 73, 74, 57, 58, 59, 66, 67, 68, 75, 76, 77, 60,
+    61, 62, 69, 70, 71, 78, 79, 80]
     chars = [' ']*81
     for i, x in enumerate(gamestate[X_LOC:X_LOC+81]):
         if x:
@@ -143,6 +142,172 @@ def total_winner(gamestate, letter):
             return True
     return False
 
+def similar(gamestate1, gamestate2):
+    symmetry_functions=(
+        flip_horizontal_axis,
+        flip_vertical_axis,
+        flip_backslash_axis,
+        flip_slash_axis,
+        identity,
+        rotate_90_degrees,
+        rotate_180_degrees,
+        rotate_270_degrees
+    )
+    for f in symmetry_functions:
+        if f(gamestate1)==gamestate2:
+            return True
+    return False
+
+def flip_horizontal_axis(gamestate):
+    new_string=''
+    for i in [X_LOC,O_LOC]:
+        for sub in range(9):
+            new_sub_num=sub+(sub<3)*6-(sub>5)*6
+            new_sub=gamestate[i+9*new_sub_num:i+9*new_sub_num+9].bin
+            for j in range(6,-1,-3):
+                new_string+=new_sub[j:j+3]
+    for i in range(6,-1,3):
+        new_string+=gamestate[PREV_MOV+i:PREV_MOV+i+3].bin
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(6,-1,3):
+        new_string+=gamestate[X_VIC+i:X_VIC+i+3].bin
+    for i in range(6,-1,3):
+        new_string+=gamestate[O_VIC+i:O_VIC+i+3].bin
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
+def flip_vertical_axis(gamestate):
+    new_string=''
+    for i in [X_LOC,O_LOC]:
+        for sub in range(9):
+            new_sub=gamestate[i+9*(sub-2*(sub%3)+2):i+9*(sub-2*(sub%3)+3)].bin
+            for j in range(9):
+                new_string+=new_sub[j+2-2*(j%3)]
+    for i in range(9):
+        new_string+=str(int(gamestate[PREV_MOV+(i+2-2*(i%3))]))
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(9):
+        new_string+=str(int(gamestate[X_VIC+(i+2-2*(i%3))]))
+    for i in range(9):
+        new_string+=str(int(gamestate[O_VIC+(i+2-2*(i%3))]))
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
+def flip_backslash_axis(gamestate):
+    new_string=''
+    for x in [X_LOC,O_LOC]:
+        for i in range(0,7,3):
+            for j in range(3):
+                new_sub=gamestate[x+9*(i+j):x+9*(i+j+1)].bin
+                for k in range(0,7,3):
+                    for l in range(3):
+                        new_string+=new_sub[k+l]
+    for i in range(0,7,3):
+        for j in range(3):
+            new_string+=str(int(gamestate[PREV_MOV+i+j]))
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(0,7,3):
+        for j in range(3):
+            new_string+=str(int(gamestate[X_VIC+i+j]))
+    for i in range(0,7,3):
+        for j in range(3):
+            new_string+=str(int(gamestate[O_VIC+i+j]))
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
+def flip_slash_axis(gamestate):
+    new_string=''
+    for x in [X_LOC,O_LOC]:
+        for i in range(8,5,-1):
+            for j in range(0,-7,-3):
+                new_sub=gamestate[x+9*(i+j):x+9*(i+j+1)].bin
+                for k in range(8,5,-1):
+                    for l in range(0,-7,-3):
+                        new_string+=new_sub[k+l]
+    for i in range(8,5,-1):
+        for j in range(0,-7,-3):
+            new_string+=str(int(gamestate[PREV_MOV+i+j]))
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(8,5,-1):
+        for j in range(0,-7,-3):
+            new_string+=str(int(gamestate[X_VIC+i+j]))
+    for i in range(8,5,-1):
+        for j in range(0,-7,-3):
+            new_string+=str(int(gamestate[O_VIC+i+j]))
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
+def identity(gamestate):
+    return Bits(bin=gamestate.bin)
+
+def rotate_90_degrees(gamestate):
+    new_string=''
+    for x in [X_LOC,O_LOC]:
+        for i in range(6,9):
+            for j in range(0,-7,-3):
+                new_sub=gamestate[x+9*(i+j):x+9*(i+j+1)].bin
+                for k in range(6,9):
+                    for l in range(0,-7,-3):
+                        new_string+=new_sub[k+l]
+    for i in range(6,9):
+        for j in range(0,-7,-3):
+            new_string+=str(int(gamestate[PREV_MOV+i+j]))
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(6,9):
+        for j in range(0,-7,-3):
+            new_string+=str(int(gamestate[X_VIC+i+j]))
+    for i in range(6,9):
+        for j in range(0,-7,-3):
+            new_string+=str(int(gamestate[O_VIC+i+j]))
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
+def rotate_180_degrees(gamestate):
+    new_string=''
+    for x in [X_LOC,O_LOC]:
+        for i in range(8,-1,-1):
+            new_sub=gamestate[x+9*i:x+9*(i+1)].bin
+            for j in range(8,-1,-1):
+                new_string+=new_sub[j]
+    for i in range(8,-1,-1):
+        new_string+=str(int(gamestate[PREV_MOV+i]))
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(8,-1,-1):
+        new_string+=str(int(gamestate[X_VIC+i]))
+    for i in range(8,-1,-1):
+        new_string+=str(int(gamestate[O_VIC+i]))
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
+def rotate_270_degrees(gamestate):
+    new_string=''
+    for x in [X_LOC,O_LOC]:
+        for i in range(2,-1,-1):
+            for j in range(0,7,3):
+                new_sub=gamestate[x+9*(i+j):x+9*(i+j+1)].bin
+                for k in range(2,-1,-1):
+                    for l in range(0,7,3):
+                        new_string+=new_sub[k+l]
+    for i in range(2,-1,-1):
+        for j in range(0,7,3):
+            new_string+=str(int(gamestate[PREV_MOV+i+j]))
+    new_string+=str(int(gamestate[TURN]))
+    for i in range(2,-1,-1):
+        for j in range(0,7,3):
+            new_string+=str(int(gamestate[X_VIC+i+j]))
+    for i in range(2,-1,-1):
+        for j in range(0,7,3):
+            new_string+=str(int(gamestate[O_VIC+i+j]))
+    new_string+=str(int(gamestate[X_W]))
+    new_string+=str(int(gamestate[O_W]))
+    return Bits(bin=new_string)
+
 if __name__=="__main__":
     game = Bits(192)
     game = make_move(game, (5,5))
@@ -167,3 +332,16 @@ if __name__=="__main__":
     print(gamestate_to_string(game))
     game = make_move(game, (3,5))
     print(gamestate_to_string(game))
+
+    game1 = Bits(192)
+    game1 = make_move(game1, (1,1))
+    game1 = make_move(game1, (1,5))
+    game1 = make_move(game1, (5,1))
+    print(gamestate_to_string(game1))
+    game2 = Bits(192)
+    game2 = make_move(game2, (9,9))
+    game2 = make_move(game2, (9,5))
+    game2 = make_move(game2, (5,9))
+    print(gamestate_to_string(game2))
+    print('This should be true:')
+    print(similar(game1,game2))
